6ac9779c8e45d63befdda6b44bb22326a97b30d5
ph3n92h3 <ph3n92h3@gmail.com>
2023-07-31 16:41:13 +0800

    create repo

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias g="gotop"
+alias h="htop"
+alias hx="helix"
+alias j="joshuto"
+alias f="fastfetch"
+alias n="nvtop"
+alias p="paru"
+
+export all_proxy=http://127.0.0.1:7890
